e618fde074bf4df66bf2e812dad9c2906399d635
sotasage <sotasota.0702@gmail.com>
2024-09-06 04:04:32 +0900

    ユニット移動カーソルまで

--- Assets/GameSceneDirector.cs
+++ Assets/GameSceneDirector.cs
@@ -38,6 +38,22 @@ public class GameSceneDirector : MonoBehaviour
         { 0, 0, 36, 37, 38, 37, 36, 0, 0 },
     };
 
+    //フィールドデータ
+    Dictionary<Vector2Int, GameObject> tiles;
+    UnitController[,] units;
+
+    //現在選択中のユニット
+    UnitController selectUnit;
+
+    //移動可能範囲
+    Dictionary<GameObject, Vector2Int> movableTiles;
+
+    //カーソルのプレハブ
+    [SerializeField] GameObject prefabCursor;
+
+    //カーソルオブジェクト
+    List<GameObject> cursors;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -52,6 +68,14 @@ public class GameSceneDirector : MonoBehaviour
         boardWidth = boardSetting.GetLength(0);
         boardHeight = boardSetting.GetLength(1);
 
+        //フィールド初期化
+        tiles = new Dictionary<Vector2Int, GameObject>();
+        units = new UnitController[boardWidth, boardHeight];
+
+        //移動可能範囲
+        movableTiles = new Dictionary<GameObject, Vector2Int>();
+        cursors = new List<GameObject>();
+
         for (int i = 0; i < boardWidth; i++)
         {
             for (int j = 0; j < boardHeight; j++)
@@ -63,8 +87,12 @@ public class GameSceneDirector : MonoBehaviour
                 //ポジション
                 Vector3 pos = new Vector3(x, 0, y);
 
+                //タイルのインデックス
+                Vector2Int tileindex = new Vector2Int(i, j);
+
                 //タイル作成
                 GameObject tile = Instantiate(prefabTile, pos, Quaternion.identity);
+                tiles.Add(tileindex, tile);
 
                 //ユニット作成
                 int type = boardSetting[i, j] % 10;
@@ -78,6 +106,12 @@ public class GameSceneDirector : MonoBehaviour
                 GameObject prefab = prefabUnits[type - 1];
                 GameObject unit = Instantiate(prefab, pos, Quaternion.Euler(90, player * 90, 0));
                 unit.AddComponent<Rigidbody>();
+
+                UnitController unitctrl = unit.AddComponent<UnitController>();
+                unitctrl.Init(player, type, tile, tileindex);
+
+                //ユニットデータセット
+                units[i, j] = unitctrl;
             }
         }
     }
@@ -85,6 +119,134 @@ public class GameSceneDirector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject tile = null;
+        UnitController unit = null;
+
+        //プレイヤー処理
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //手前のユニットにも当たり判定があるのでヒットしたすべてのオブジェクト情報を取得
+            foreach (RaycastHit hit in Physics.RaycastAll(ray))
+            {
+                UnitController hitunit = hit.transform.GetComponent<UnitController>();
+
+                //持ち駒
+                if (hitunit && FieldStatus.Captured == hitunit.FieldStatus)
+                {
+                    unit = hitunit;
+                }
+                //タイル選択と上に乗っているユニット
+                else if (tiles.ContainsValue(hit.transform.gameObject))
+                {
+                    tile = hit.transform.gameObject;
+                    //タイルからユニットを探す
+                    foreach (var item in tiles)
+                    {
+                        if (item.Value == tile)
+                        {
+                            unit = units[item.Key.x, item.Key.y];
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        //何も選択されていなければ処理をしない
+        if (null == tile && null == unit) return;
+
+        //移動先選択
+        if (tile && selectUnit && movableTiles.ContainsKey(tile))
+        {
+            moveUnit(selectUnit, movableTiles[tile]);
+            selectUnit = null;
+        }
+
+        //ユニット選択
+        if (unit)
+        {
+            setSerectCursors(unit);
+        }
+    }
+
+    //選択時
+    void setSerectCursors(UnitController unit = null, bool playerunit = true)
+    {
+        //カーソル削除
+        foreach (var item in cursors)
+        {
+            Destroy(item);
+        }
+        cursors.Clear();
+
+        //選択ユニットの非選択状態
+        if (selectUnit)
+        {
+            selectUnit.Select(false);
+            selectUnit = null;
+        }
+
+        //ユニット情報がなければ終了
+        if (!unit) return;
+
+        //移動可能範囲取得
+        List<Vector2Int> movabletiles = getMovableTiles(unit);
+        movableTiles.Clear();
+
+        foreach (var item in movabletiles)
+        {
+            movableTiles.Add(tiles[item], item);
+
+            //カーソル生成
+            Vector3 pos = tiles[item].transform.position;
+            pos.y += 0.51f;
+            GameObject cursor = Instantiate(prefabCursor, pos, Quaternion.identity);
+            cursors.Add(cursor);
+        }
+
+        //選択状態
+        if (playerunit)
+        {
+            unit.Select();
+            selectUnit = unit;
+        }
+    }
+
+    //ユニット移動
+    void moveUnit(UnitController unit, Vector2Int tileindex)
+    {
+        //現在地
+        Vector2Int oldpos = unit.Pos;
+
+        //ユニット移動
+        unit.Move(tiles[tileindex], tileindex);
+
+        //内部データ更新(新しい場所)
+        units[tileindex.x, tileindex.y] = unit;
+
+        //ボード上の駒を更新
+        if (FieldStatus.OnBoard == unit.FieldStatus)
+        {
+            //内部データ更新
+            units[oldpos.x, oldpos.y] = null;
+        }
+        //持ち駒の更新
+        else
+        {
+            //持ち駒の更新
+        }
+
+        //ユニットの状態を更新
+        unit.FieldStatus = FieldStatus.OnBoard;
+    }
+
+    List<Vector2Int> getMovableTiles(UnitController unit)
+    {
+        List<Vector2Int> ret = unit.GetMovableTiles(units);
+
+        //王手されてしまうかチェック
+
+        return ret;
     }
 }
---
+++ Assets/UnitController.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+//駒のタイプ
+public enum UnitType
+{
+    None=-1,
+    Hu= 1,
+    Kaku,
+    Hisha,
+    Kyousha,
+    Keima,
+    Gin,
+    Kin,
+    Gyoku,
+    //成り
+    Tokin,
+    Uma,
+    Ryu,
+    NariKyo,
+    Narikei,
+    Narigin,
+}
+
+//駒の場所
+public enum FieldStatus
+{
+    OnBoard,
+    Captured,
+}
+
+
+
+public class UnitController : MonoBehaviour
+{
+    //ユニットのプレイヤー番号 1P: 0, 2P: 1, 3P: 2, 4P: 3
+    public int Player;
+    //ユニットの種類
+    public UnitType UnitType, OldUnitType;
+    //ユニットの場所
+    public FieldStatus FieldStatus;
+
+    //成りテーブル
+    Dictionary<UnitType, UnitType> evolutionTable = new Dictionary<UnitType, UnitType>()
+    {
+        {UnitType.Hu, UnitType.Tokin },
+        {UnitType.Kaku, UnitType.Uma },
+        {UnitType.Hisha, UnitType.Ryu },
+        {UnitType.Keima, UnitType.Narikei },
+        {UnitType.Kyousha, UnitType.NariKyo },
+        {UnitType.Gin, UnitType.Narigin },
+        {UnitType.Kin, UnitType.None},
+        {UnitType.Gyoku, UnitType.None },
+
+    };
+
+    //成り済みかどうか
+    public bool isEvolution;
+
+    //ユニットの選択/非選択のy座標
+    public const float SelectUnitY = 1.5f;
+    public const float UnSelectUnitY = 0.7f;
+
+    //おいている場所
+    public Vector2Int Pos;
+
+    //選択される前のy座標
+    float oldPosY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //初期設定
+    public void Init(int player,int unittype,GameObject tile, Vector2Int pos)
+    {
+        Player = player;
+        UnitType = (UnitType)unittype;
+        //獲られたときもとにもどるよう
+        OldUnitType = (UnitType)unittype;
+        //場所の初期化
+        FieldStatus = FieldStatus.OnBoard;
+        //確度と場所
+        transform.eulerAngles = getDefaultAngles(player);
+        Move(tile, pos);
+
+    }
+    //指定されたプレイヤー番号の向きを返す
+    Vector3 getDefaultAngles(int player)
+    {
+        return new Vector3(90, player * 90, 0);
+    }
+
+    //移動する処理
+    public void Move(GameObject tile, Vector2Int tileindex)
+    {
+        //新しい場所に非選択状態で移動する
+        Vector3 pos = tile.transform.position;
+        pos.y = UnSelectUnitY;
+        transform.position = pos;
+
+        Pos = tileindex;
+    }
+
+    public void Select(bool select = true)
+    {
+        Vector3 pos = transform.position;
+        bool iskinematic = select;
+
+        if (select)
+        {
+            oldPosY = pos.y;
+            pos.y = SelectUnitY;
+        }
+        else
+        {
+            pos.y = UnSelectUnitY;
+
+            //持ちゴマの位置は特別
+            if (FieldStatus.Captured == FieldStatus)
+            {
+                pos.y = oldPosY;
+                iskinematic = true;
+            }
+        }
+
+        GetComponent<Rigidbody>().isKinematic = iskinematic;
+        transform.position = pos;
+    }
+
+    //移動可能範囲取得
+    public List<Vector2Int> GetMovableTiles(UnitController[,] units, bool checkotherunit = true)
+    {
+        List<Vector2Int> ret = new List<Vector2Int>();
+
+        ret = GetMovableTiles(units, UnitType.Hu);
+
+        return ret;
+    }
+
+    List<Vector2Int> GetMovableTiles(UnitController[,] units, UnitType unittype)
+    {
+        List<Vector2Int> ret = new List<Vector2Int>();
+
+        //歩
+        if (UnitType.Hu == unittype)
+        {
+            //4人用なので向きが4方向になる 左回りにターンが進む
+            //向き
+            Vector2Int dir = new Vector2Int(0, 1);
+
+            if (Player == 1) dir = new Vector2Int(1, 0);
+            if (Player == 2) dir = new Vector2Int(0, -1);
+            if (Player == 3) dir = new Vector2Int(-1, 0);
+
+            //前方1マス
+            List<Vector2Int> vec = new List<Vector2Int>()
+            {
+                dir
+            };
+
+            foreach (var item in vec)
+            {
+                Vector2Int checkpos = Pos + item;
+                if (!isCheckable(units, checkpos) || isFriendlyUnit(units[checkpos.x, checkpos.y])) continue;
+                ret.Add(checkpos);
+            }
+        }
+
+        return ret;
+    }
+
+    //配列オーバーかどうか
+    bool isCheckable(UnitController[,] ary, Vector2Int idx)
+    {
+        //配列オーバーの状態
+        if (idx.x < 0 || ary.GetLength(0) <= idx.x || idx.y < 0 || ary.GetLength(1) <= idx.y)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    //仲間のユニットかどうか
+    bool isFriendlyUnit(UnitController unit)
+    {
+        if (unit && Player == unit.Player) return true;
+        return false;
+    }
+}
